933116e7a14ea72574d38798d164eb9c8d25db07
matthewdau85 <matthew.donovan@alumni.griffithuni.edu.au>
2025-11-17 05:13:27 +1000

    Backfill onboarding flag for legacy orgs

--- shared/prisma/migrations/20251201090000_add_org_onboarding_flag/migration.sql
+++ shared/prisma/migrations/20251201090000_add_org_onboarding_flag/migration.sql
@@ -1,5 +1,11 @@
 ALTER TABLE "Organization"
 ADD COLUMN IF NOT EXISTS "onboardingComplete" BOOLEAN NOT NULL DEFAULT FALSE;
+UPDATE "Organization"
+SET "onboardingComplete" = TRUE
+WHERE "onboardingComplete" = FALSE;
 
 ALTER TABLE "Org"
 ADD COLUMN IF NOT EXISTS "onboardingComplete" BOOLEAN NOT NULL DEFAULT FALSE;
+UPDATE "Org"
+SET "onboardingComplete" = TRUE
+WHERE "onboardingComplete" = FALSE;
