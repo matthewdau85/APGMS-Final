08e0b660f28cf04291d75d4fd98ae5dead142f84
matthewdau85 <matthew.donovan@alumni.griffithuni.edu.au>
2025-11-24 03:27:44 +1000

    Restrict Jest to test suites

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,10 @@ export default {
     ],
   },
 
-  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+  testMatch: ["**/?(*.)test.[tj]s?(x)"],
+  testPathIgnorePatterns: [
+    "<rootDir>/services/api-gateway/test/.*\\.spec\\.[tj]s?$",
+  ],
 
   // Resolve monorepo imports like @apgms/shared
   moduleNameMapper: {
